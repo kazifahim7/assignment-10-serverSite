66be09be95a79cfbf10afeed807bfcb71e633c61
kazi fahim <kazifahim661@gmail.com>
2024-04-26 18:52:13 +0600

    env folder created

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
+        
         // await client.connect();
         const database = client.db("spotsDB");
         const spotCollection = database.collection("spotCollection");
